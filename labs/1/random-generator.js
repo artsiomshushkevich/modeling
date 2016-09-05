7665f51bf9a28a11b9e2ad45c8a81831f51ac7b3
Artsiom Shushkevich <Artsiom_Shushkevich@epam.com>
2016-09-05 14:14:52 +0300

    add correct hostogram view

--- labs/1/random-generator.js
+++ labs/1/random-generator.js
@@ -1,46 +1,51 @@
 
 (function(){
   
-  var r0 = 1;
-  var m = 5;
-  var a = 3;
-  var n = 5;
-  var deltaInterval = 0.2;
+  var r0 = 2836;
+  var m = 2147483647;
+  var a = 16807;
+  var n = 10000;
+//  var deltaInterval = 0.2;
   var arrayOfRandomNumbers = [];
+  
   var generateButton = document.querySelector('#generate-but');
   generateButton.addEventListener('click', showHistrogram);
   
-  function getArrayOfPenetrationsAmounts() {
-    var currentIntervalValue = 0;
-    var arrayOfPenetrationsAmounts = [];
-    
-    while (currentIntervalValue < 1) {
-      arrayOfPenetrationsAmounts.push(0);
-      
-      for (var i = 0; i < arrayOfRandomNumbers.length; i++) {
-        if (arrayOfRandomNumbers[i] > currentIntervalValue && 
-            arrayOfRandomNumbers[i] < (currentIntervalValue + deltaInterval)) {
-          arrayOfPenetrationsAmounts[arrayOfPenetrationsAmounts.length - 1]++;
-        }
-        
-      }
-       currentIntervalValue += deltaInterval;
-    }
-    
-    return arrayOfPenetrationsAmounts;
-  }
-  
-  function getArrayOfIntervals() {
-    var currentIntervalValue = 0;
-    var tempArrayOfIntervals = [];
-    
-    while (currentIntervalValue < 1) {
-      tempArrayOfIntervals.push(currentIntervalValue);
-      currentIntervalValue += deltaInterval;
-    }
-    
-    return tempArrayOfIntervals;
-  }
+//  function getArrayOfPenetrationsAmounts() {
+//    var currentIntervalValue = 0;
+//    var arrayOfPenetrationsAmounts = [];
+//    
+//    while (currentIntervalValue < 1) {
+//      arrayOfPenetrationsAmounts.push(0);
+//      
+//      for (var i = 0; i < arrayOfRandomNumbers.length; i++) {
+//        if (arrayOfRandomNumbers[i] >= currentIntervalValue && 
+//            arrayOfRandomNumbers[i] < (currentIntervalValue + deltaInterval)) {
+//          arrayOfPenetrationsAmounts[arrayOfPenetrationsAmounts.length - 1]++;
+//        }
+//        
+//      }
+//      
+//      currentIntervalValue += deltaInterval;
+//      currentIntervalValue = +currentIntervalValue.toFixed(2);
+//    }
+//    
+//    return arrayOfPenetrationsAmounts;
+//  }
+//  
+//  function getArrayOfIntervals() {
+//    var currentIntervalValue = 0;
+//    var tempArrayOfIntervals = [];
+//    
+//    while (currentIntervalValue < 1) {
+//      tempArrayOfIntervals.push(currentIntervalValue);
+//      currentIntervalValue += deltaInterval;
+//      currentIntervalValue = +currentIntervalValue.toFixed(2);
+//    }
+//    
+//    return tempArrayOfIntervals;
+//  }
+//  
   
   function showHistrogram() {
     if (arrayOfRandomNumbers.length > 0) {
@@ -49,31 +54,26 @@
     
     lemerFunction(r0, n);
     
-    var arrayOfPenetrationsAmounts = getArrayOfPenetrationsAmounts();
-    var arrayOfRelativeFrequencies = arrayOfPenetrationsAmounts.map(function(number) {
-      return number / arrayOfRandomNumbers.length;
-    });
-    
-    var arrayOfIntervals = getArrayOfIntervals();
-    
-    var data = [
-      {
-        x: [0, 0.2, 0.4, 0.6, 0.8, 1],
-        y: arrayOfRelativeFrequencies,
-        type: 'histogram',
-        marker: {
-          color: 'rgba(100,250,100,0.7)',
-        },
-      } 
-    ];
+//    var arrayOfPenetrationsAmounts = getArrayOfPenetrationsAmounts();
+//    var arrayOfRelativeFrequencies = arrayOfPenetrationsAmounts.map(function(number) {
+//      return number / arrayOfRandomNumbers.length;
+//    });
+//    
+//    var arrayOfIntervals = getArrayOfIntervals();
+  
+    var data = [{
+      x: arrayOfRandomNumbers,
+      type: 'histogram'
+    }];
     
     Plotly.newPlot('histogram', data); 
   } 
-  
+
   function lemerFunction(prevR, n) {
     if (n > 0) {
       var rN = prevR * a % m;
       var r = rN / m;
+
       arrayOfRandomNumbers.push(r);
       lemerFunction(rN, --n);
     }
