5b4dd47f342c287ee4272cb2914339f4ac4e9db5
Artsiom Shushkevich <artemshushkevich@live.com>
2016-09-06 00:45:59 +0300

    Add dispersion, standard deviation, expected value

--- labs/1/random-generator.js
+++ labs/1/random-generator.js
@@ -4,12 +4,12 @@
   var r0 = 2836;
   var m = 2147483647;
   var a = 16807;
-  var n = 10000;
+  var n = 25000;
 //  var deltaInterval = 0.2;
   var arrayOfRandomNumbers = [];
   
   var generateButton = document.querySelector('#generate-but');
-  generateButton.addEventListener('click', showHistrogram);
+  generateButton.addEventListener('click', showResults);
   
 //  function getArrayOfPenetrationsAmounts() {
 //    var currentIntervalValue = 0;
@@ -47,7 +47,9 @@
 //  }
 //  
   
-  function showHistrogram() {
+  
+  
+  function showResults() {
     if (arrayOfRandomNumbers.length > 0) {
       arrayOfRandomNumbers = [];
     }
@@ -66,9 +68,61 @@
       type: 'histogram'
     }];
     
-    Plotly.newPlot('histogram', data); 
+    window.Plotly.newPlot('histogram', data); 
+    
+    var expectedValue = getExpectedValue();
+    var dispersion = getDispersion(expectedValue);
+    var standardDeviation = getStandardDeviation(dispersion);
+    
+    var otherResultsContainer = document.querySelector('#other-results');
+    otherResultsContainer.innerHTML = `
+      <p>Expected value: ${expectedValue}</p>
+      <p>Dispersion: ${dispersion}</p>
+      <p>Standard deviation: ${standardDeviation}</p>
+    `;
+    
+    (function ()  {
+      var flag = false;
+      for (var i = 0; i < arrayOfRandomNumbers.length - 1; i++) {
+        
+        for (var j = i + 1; j < arrayOfRandomNumbers.length; j++) {
+          if ((arrayOfRandomNumbers[i]) === (arrayOfRandomNumbers[j])) {
+            flag = true;
+            break;
+          }
+        }
+        if (flag) {
+          break;
+        }
+      }
+      if (!flag) {
+        alert('there is not');
+      } else {
+        alert('there is');
+      }
+    })();
   } 
-
+  
+  function getExpectedValue() {
+    return arrayOfRandomNumbers.reduce(function(prevValue, curValue) {
+      return prevValue + curValue
+    }, 0) / arrayOfRandomNumbers.length;
+  }
+  
+  function getDispersion(expectedValue) {
+    var tempDispersionSum = 0;
+    
+    for (var i = 0; i < arrayOfRandomNumbers.length; i++) {
+      tempDispersionSum += Math.pow(arrayOfRandomNumbers[i] - expectedValue, 2);
+    }
+    
+    return tempDispersionSum * (1 / (arrayOfRandomNumbers.length - 1));
+  }
+  
+  function getStandardDeviation(dispersion) {
+    return Math.sqrt(dispersion / arrayOfRandomNumbers.length);
+  }
+  
   function lemerFunction(prevR, n) {
     if (n > 0) {
       var rN = prevR * a % m;
