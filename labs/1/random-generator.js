6d856ab7e987b1d16a12c1cfa9c9f72325bb77af
Artsiom Shushkevich <Artsiom_Shushkevich@epam.com>
2016-09-06 17:48:22 +0300

    refactor code

--- labs/1/random-generator.js
+++ labs/1/random-generator.js
@@ -1,78 +1,30 @@
 
 (function(){
-  
   var r0 = 2836;
   var m = 2147483647;
   var a = 16807;
-  var n = 25000;
-//  var deltaInterval = 0.2;
-  var arrayOfRandomNumbers = [];
+  var n = 50000;
+  
+//  var r0 = 61;
+//  var m = 64;
+//  var a = 63;
+//  var n = 10;
   
   var generateButton = document.querySelector('#generate-but');
   generateButton.addEventListener('click', showResults);
-  
-//  function getArrayOfPenetrationsAmounts() {
-//    var currentIntervalValue = 0;
-//    var arrayOfPenetrationsAmounts = [];
-//    
-//    while (currentIntervalValue < 1) {
-//      arrayOfPenetrationsAmounts.push(0);
-//      
-//      for (var i = 0; i < arrayOfRandomNumbers.length; i++) {
-//        if (arrayOfRandomNumbers[i] >= currentIntervalValue && 
-//            arrayOfRandomNumbers[i] < (currentIntervalValue + deltaInterval)) {
-//          arrayOfPenetrationsAmounts[arrayOfPenetrationsAmounts.length - 1]++;
-//        }
-//        
-//      }
-//      
-//      currentIntervalValue += deltaInterval;
-//      currentIntervalValue = +currentIntervalValue.toFixed(2);
-//    }
-//    
-//    return arrayOfPenetrationsAmounts;
-//  }
-//  
-//  function getArrayOfIntervals() {
-//    var currentIntervalValue = 0;
-//    var tempArrayOfIntervals = [];
-//    
-//    while (currentIntervalValue < 1) {
-//      tempArrayOfIntervals.push(currentIntervalValue);
-//      currentIntervalValue += deltaInterval;
-//      currentIntervalValue = +currentIntervalValue.toFixed(2);
-//    }
-//    
-//    return tempArrayOfIntervals;
-//  }
-//  
-  
-  
-  
+    
   function showResults() {
-    if (arrayOfRandomNumbers.length > 0) {
-      arrayOfRandomNumbers = [];
-    }
+    var arrayOfRandomNumbers = lemerFunction(r0);
+    var expectedValue = getExpectedValue(arrayOfRandomNumbers);
+    var dispersion = getDispersion(expectedValue, arrayOfRandomNumbers);
+    var standardDeviation = getStandardDeviation(dispersion, arrayOfRandomNumbers);
     
-    lemerFunction(r0, n);
     
-//    var arrayOfPenetrationsAmounts = getArrayOfPenetrationsAmounts();
-//    var arrayOfRelativeFrequencies = arrayOfPenetrationsAmounts.map(function(number) {
-//      return number / arrayOfRandomNumbers.length;
-//    });
-//    
-//    var arrayOfIntervals = getArrayOfIntervals();
-  
     var data = [{
       x: arrayOfRandomNumbers,
       type: 'histogram'
     }];
-    
     window.Plotly.newPlot('histogram', data); 
-    
-    var expectedValue = getExpectedValue();
-    var dispersion = getDispersion(expectedValue);
-    var standardDeviation = getStandardDeviation(dispersion);
     
     var otherResultsContainer = document.querySelector('#other-results');
     otherResultsContainer.innerHTML = `
@@ -80,36 +32,31 @@
       <p>Dispersion: ${dispersion}</p>
       <p>Standard deviation: ${standardDeviation}</p>
     `;
-    
-    (function ()  {
-      var flag = false;
-      for (var i = 0; i < arrayOfRandomNumbers.length - 1; i++) {
-        
-        for (var j = i + 1; j < arrayOfRandomNumbers.length; j++) {
-          if ((arrayOfRandomNumbers[i]) === (arrayOfRandomNumbers[j])) {
-            flag = true;
-            break;
-          }
-        }
-        if (flag) {
-          break;
-        }
-      }
-      if (!flag) {
-        alert('there is not');
-      } else {
-        alert('there is');
-      }
-    })();
   } 
   
-  function getExpectedValue() {
+  function getPeriod(arrayOfRandomNumbers) {
+    var period = -1;
+    
+    for (var i = 0; i < arrayOfRandomNumbers.length - 1 ; i++) {
+      var indexOfOccurence = arrayOfRandomNumbers.indexOf(arrayOfRandomNumbers[i], i + 1);
+
+      if (arrayOfRandomNumbers.indexOf(arrayOfRandomNumbers[i], i + 1) !== -1) {
+        period = indexOfOccurence - i;
+        
+        return period;
+      }
+    }
+      
+    return period;  
+  }
+  
+  function getExpectedValue(arrayOfRandomNumbers) {
     return arrayOfRandomNumbers.reduce(function(prevValue, curValue) {
-      return prevValue + curValue
+      return prevValue + curValue;
     }, 0) / arrayOfRandomNumbers.length;
   }
   
-  function getDispersion(expectedValue) {
+  function getDispersion(expectedValue, arrayOfRandomNumbers) {
     var tempDispersionSum = 0;
     
     for (var i = 0; i < arrayOfRandomNumbers.length; i++) {
@@ -119,19 +66,22 @@
     return tempDispersionSum * (1 / (arrayOfRandomNumbers.length - 1));
   }
   
-  function getStandardDeviation(dispersion) {
+  function getStandardDeviation(dispersion, arrayOfRandomNumbers) {
     return Math.sqrt(dispersion / arrayOfRandomNumbers.length);
   }
   
-  function lemerFunction(prevR, n) {
-    if (n > 0) {
+  function lemerFunction(prevR) {
+    var arrayOfRandomNumbers = [];
+    
+    while (n > 0) {
       var rN = prevR * a % m;
       var r = rN / m;
-
+      prevR = rN;
+      
       arrayOfRandomNumbers.push(r);
-      lemerFunction(rN, --n);
+      n--;
     }
-  }
-  
-
+    
+    return arrayOfRandomNumbers;
+  } 
 })(document, window);
