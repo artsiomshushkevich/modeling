69c7fe598b5f1df00bc299db45173244dfa5ce96
Artsiom Shushkevich <artemshushkevich@live.com>
2016-11-04 14:30:27 +0300

    fix some bugs in app and add dip

--- labs/lab4/app/index.js
+++ labs/lab4/app/index.js
@@ -1,19 +1,21 @@
 (function() {
-  var mu, lamda, p, j, p1, amountOfRequestsOfFirstSort, amountOfRequestsOfSecondSort, q1, q2;
+  var mu, lambda, p, 
+      queue, channel,
+      q1, q2, 
+      amountOfMissedRequestsOfSecondSort, amountOfMissedRequestsOfSecondSort, 
+      timeOfGeneratingRequest, timeOfResolvingRequest, 
+      generatingRequestCounter, resolvingRequestCounter,
+      amountOfRequestsOfFirstSort, amountOfRequestsOfSecondSort;
+
   var COUNT_OF_TICKS = 20000;
-  var counter;
-  var tempCounter;
 
   $('#count-params-but').on('click', countParams);
 
   function countParams() {
-    clearAll();
-
     mu = +$('#mu').val();
-    lambda = +$('#lambda').val()
+    lambda = +$('#lambda').val();
     p = +$('#p').val();
-    counter  = Math.floor(1/mu);
-    tempCounter = counter;
+
     if (p > 1) {
       alert('P more than 1');
       clearAll();
@@ -21,72 +23,110 @@
       return;
     }
 
+    clearAll();
+
     for (var i = 0; i < COUNT_OF_TICKS; i++) {
       goToNextState();
     }
 
-    q1 = 1 - amountOfRequestsOfFirstSort / COUNT_OF_TICKS;
-    q2 = 1 - amountOfRequestsOfSecondSort / COUNT_OF_TICKS;
-    
+    q1 = 1 - amountOfMissedRequestsOfFirstSort / amountOfRequestsOfFirstSort;
+    q2 = 1 - amountOfMissedRequestsOfSecondSort / amountOfRequestsOfSecondSort;
+
     $('#results').append(`
       <p>Q1 = ${q1}</p>
       <p>Q2 = ${q2}</p>
     `);
- }
+  }
 
- function clearAll() {
-   j = 0;
-   p1 = 0; 
-   amountOfRequestsOfFirstSort = 0;
-   amountOfRequestsOfSecondSort = 0;
-   q1 = 0;
-   q2 = 0;
-   
-  //  $('input').val('');
-   $('#results').empty();
- }
+  function clearAll() {
+    queue = 0;
+    channel = 0; 
+    amountOfMissedRequestsOfFirstSort = 0;
+    amountOfMissedRequestsOfSecondSort = 0;
+    amountOfRequestsOfFirstSort = 0;
+    amountOfRequestsOfSecondSort = 0;
+    q1 = 0;
+    q2 = 0;
+    timeOfGeneratingRequest = getNumberByExponencialDistribution(lambda);
+    generatingRequestCounter = 0;
+    resolvingRequestCounter = -1;
+  
+    $('#results').empty();
+  }
                                            
- function isRequestProcessed() {
-   return Math.random() < mu;
- }
+  function getNumberByExponencialDistribution(number) {
+    return -(1 / number) * Math.log(Math.random());
+  }
 
- function isRequestOfFirstSort() {
-   return Math.random() < p;
- }
+  function isRequestOfFirstSort() {
+    return getNumberByExponencialDistribution(Math.random()) < p;
+  }
 
- function goToNextState() {
-   if (p1 !== 0) {
-     if (isRequestProcessed()) {
-      //  amountOfRequestsOfFirstSort++;
-       p1 = 0;
+  function goToNextState() {
+    if (resolvingRequestCounter !== -1) {
+      resolvingRequestCounter++;
+    }
 
-       if (j !== 0) {
-         p1 = j;
-         j = 0;
-       }
-     }
-   }
+    generatingRequestCounter++;
 
-   if (isRequestOfFirstSort()) {
-     if (j === 1) {
-       amountOfRequestsOfFirstSort++;
-     } else if (j === 2) {
-       amountOfRequestsOfSecondSort++;
-       j === 1;
-     } else if(j === 0 && p1 !== 0)  {
-       j = 1;
-     } else {
-       p1 = 1;
-     }
-       
-   } else {
-     if (j !== 0) {
-       amountOfRequestsOfSecondSort++;
-     } else if (j === 0 && p1 !== 0)  {
-       j = 2;
-     } else {
-       p1 = 2;
-     }
-   }
- }
+    if (resolvingRequestCounter > timeOfResolvingRequest) {
+      if (queue !== 0) {
+        channel = queue;
+        queue = 0;
+        resolvingRequestCounter = 0;
+      } else {
+        queue = 0;
+        channel = 0;
+        resolvingRequestCounter = -1;
+      }
+    }
+
+    if (generatingRequestCounter > timeOfGeneratingRequest) {
+      if (isRequestOfFirstSort()) {
+        amountOfRequestsOfFirstSort++;
+
+        if (channel === 0) {
+          channel = 1;
+          resolvingRequestCounter++;
+          timeOfResolvingRequest = getNumberByExponencialDistribution(mu);
+        } else if (channel === 1) {
+          if (queue === 0) {
+            queue = 1;
+          } else if (queue === 1) {
+            amountOfMissedRequestsOfFirstSort++
+          } else {
+            queue = 1;
+            amountOfMissedRequestsOfSecondSort++;
+          }
+        } else {
+          if (queue === 0) {
+            queue = 2;       
+          } else {
+            amountOfMissedRequestsOfSecondSort++;  
+          }
+
+          channel = 1;
+          resolvingRequestCounter = 0;
+          timeOfResolvingRequest = getNumberByExponencialDistribution(mu);
+        } 
+      } else {
+        amountOfRequestsOfSecondSort++;
+
+        if (channel === 0) {
+          channel = 2;
+          resolvingRequestCounter++;
+          timeOfResolvingRequest = getNumberByExponencialDistribution(mu);
+        } else {
+          if (queue !== 0) {
+            amountOfMissedRequestsOfSecondSort++;
+          } else {
+            queue = 2;
+          }
+        }
+      }
+
+      generatingRequestCounter = 0;
+      timeOfGeneratingRequest = getNumberByExponencialDistribution(lambda);
+    }
+  }
 })();
